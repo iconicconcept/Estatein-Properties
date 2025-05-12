f51b4b1b7cec4561a7db7dfd381f5fce02ac9cf2
Mubaraq Allamalyekeen <mubaraqadeniyi159@gmail.com>
2025-05-12 12:42:58 +0100

    again

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -20,15 +20,13 @@ const Footer = () => {
         <section className='bg-gray-950 py-7 md:py-10 lg:py-19 px-5 md:px-30 lg:px-30 flex flex-col md:flex-row lg:flex-row md:justify-between lg:justify-between gap-11 md:w-full lg-w-full'>
             <div className='flex flex-col gap-3' data-aos="fade-up">
                 <img src={logo} alt="logo" className='h-[40px] w-[100px]' />
-                <input type="text" id='gmail' name="gmail" onChange={change} className='email px-3 py-3 border border-gray-500 rounded text-white w-85 md:w-80 lg:w-80 md:py-2 lg:py-2' placeholder='Enter your Email' />
+                <input type="text" id='gmail' name="gmail" onChange={change} className='email px-3 py-3 border border-gray-500 rounded text-white w-[100%] md:w-80 lg:w-80 md:py-2 lg:py-2' placeholder='Enter your Email' />
                 <button onClick={sentEmail} className='cursor-pointer px-4 py-2 text-white rounded bg-gray-800 text-[14px] w-max'>Submit</button>
             </div>
             <div className='grid grid-cols-2 md:flex lg:flex md:gap-2 lg:gap-2' data-aos="fade-right">
                 <div className='flex flex-col md:flex-row lg:flex-row gap-4 border md:border-0 lg:border-0 border-r-gray-500 '>
                     <ul className='text-white text-[13px] flex flex-col gap-2 py-4 border-b md:border-0 lg:border-0 mr-4'>
                         <li className='mb-2 text-gray-500 font-bold'>Home</li>
-                        {/*ffjhgjjkkk hkhhk,n,n*/}
-                                                {/*ffjhgjjkkk hkhhk,n,n*/}
                         <li className='cursor-pointer hover:underline hover:underline-offset-3'><Link to="/">Hero Section</Link></li>
                         <li className='cursor-pointer hover:underline hover:underline-offset-3'><Link to="/about">Features</Link></li>
                         <li className='cursor-pointer hover:underline hover:underline-offset-3'><Link to="/properties">Properties</Link></li>
